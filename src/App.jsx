7ed37d0a94b88f9be3e461df974017a9d62f86ad
TheusMarqs <matheusmarqsilva@gmail.com>
2024-02-26 15:05:49 -0300

    react inicio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='app'>
+    <h1>Programação Multiplataforma</h1>
+    <h2>Teste de react js</h2>
+    <p>Aguarde novas versões</p>
+   </div>
+  )
+}
+
+export default App
